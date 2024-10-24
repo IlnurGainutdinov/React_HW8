9af041f8a59f970c2b86bfbaec5e05e554911756
IlnurGainutdinov <gainutdinov86@list.ru>
2024-10-24 17:32:43 +0300

    First commit

---
+++ src/components/Single/Single.js
@@ -0,0 +1,16 @@
+import Product from "./Elements/Product";
+import Collection from "./Elements/Collection";
+import YouMayLike from "./Elements/YouMayLike";
+import Arrivals from "../Arrivals";
+
+const Single = () => {
+  return (
+    <div>
+      <Arrivals />
+      <Product />
+      <Collection />
+      <YouMayLike />
+    </div>
+  );
+};
+export default Single;
